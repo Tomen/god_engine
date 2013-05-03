88407a2750aa5918ca7e8cfb95f19c88744ad713
Tommi Enenkel <tommi.enenkel@gmail.com>
2013-05-04 00:03:50 +0200

    add network to incubator

--- lib/god_engine.dart
+++ lib/god_engine.dart
@@ -5,13 +5,13 @@ import 'dart:html';
 import 'package:vector_math/vector_math.dart';
 import 'package:game_loop/game_loop_html.dart';
 
-part 'src/core/entity.dart';
-part 'src/core/scene.dart';
-part 'src/core/component.dart';
+part 'core/entity.dart';
+part 'core/scene.dart';
+part 'core/component.dart';
 
-part 'src/graphics/scene_renderer.dart';
-part 'src/graphics/graphics_component.dart';
-part 'src/graphics/camera_component.dart';
+part 'graphics/scene_renderer.dart';
+part 'graphics/graphics_component.dart';
+part 'graphics/camera_component.dart';
 
-part 'src/physics/physics_simulator.dart';
-part 'src/physics/physics_component.dart';
+part 'physics/physics_simulator.dart';
+part 'physics/physics_component.dart';
